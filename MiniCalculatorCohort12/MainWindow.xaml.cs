8241193a718682bc622e979d62d346f3b51a4618
Kawser Wazed Nafi <kwnafi@yahoo.com>
2023-11-16 11:06:24 -0500

    Add files via upload

---
+++ MiniCalculatorCohort12/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using MiniCalculatorCohort12.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MiniCalculatorCohort12
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Calculator calc = new Calculator();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Addition_Click(object sender, RoutedEventArgs e)
+        {
+            double a = double.Parse(inpA.Text);
+            double b = double.Parse(inpB.Text);
+
+            Results.Text = calc.getAddition(a, b).ToString();
+        }
+
+        private void Substraction_Click(object sender, RoutedEventArgs e)
+        {
+            double a = double.Parse(inpA.Text);
+            double b = double.Parse(inpB.Text);
+
+            Results.Text = calc.getSubstraction(a,b).ToString();
+        }
+
+        private void Multiplication_Click(object sender, RoutedEventArgs e)
+        {
+            double a = double.Parse(inpA.Text);
+            double b = double.Parse(inpB.Text);
+
+            Results.Text = calc.getMultiplication(a,b).ToString();  
+        }
+
+        private void Division_Click(object sender, RoutedEventArgs e)
+        {
+            double a = double.Parse(inpA.Text);
+            double b = double.Parse(inpB.Text);
+
+            Results.Text = calc.getDivision(a,b).ToString();
+        }
+
+        private void Modulus_Click(object sender, RoutedEventArgs e)
+        {
+            double a = double.Parse(inpA.Text);
+            double b = double.Parse(inpB.Text);
+
+            Results.Text = calc.getModulus(a,b).ToString();
+        }
+    }
+}
---
+++ MiniCalculatorCohort12/Models/Calculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniCalculatorCohort12.Models
+{
+    public class Calculator
+    {
+        public double getAddition(double a, double b)
+        {
+            return a + b;
+        }
+        public double getSubstraction(double a, double b)
+        {
+            return a - b;
+        }
+        public double getMultiplication(double a, double b)
+        {
+            return a * b;
+        }
+        public double getDivision(double a, double b)
+        {
+            //return a % b; // accidentally put it
+            return a / b;
+        }
+        public double getModulus(double a, double b)
+        {
+            //return a / b;
+            return a % b;
+        }
+    }
+}
